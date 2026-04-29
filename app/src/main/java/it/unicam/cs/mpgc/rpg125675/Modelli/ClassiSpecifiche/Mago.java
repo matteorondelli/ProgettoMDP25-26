a0a414b650503323f25e25186ab19e93040ddae1
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-04-29 17:23:32 +0200

    inizializzazione superclasse ClasseGiocatore che estende le due specializzazioni Cavaliere e mago

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/ClassiSpecifiche/Mago.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.mpgc.rpg125675.Modelli.ClassiSpecifiche;
+
+import it.unicam.cs.mpgc.rpg125675.Modelli.Personaggio;
+
+public class Mago extends ClasseGiocatore {
+    public int getHp() { return 100; }
+    public int getAttack() { return 25; }
+}
