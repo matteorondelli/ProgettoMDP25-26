303329e38a9bddd9961e8543ff58185e0ae24d82
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-05 16:38:19 +0200

    Eliminazione classi

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/ClassiSpecifiche/Cavaliere.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg125675.Modelli.ClassiSpecifiche;
-
-import it.unicam.cs.mpgc.rpg125675.Modelli.Personaggio;
-
-public class Cavaliere extends ClasseGiocatore {
-
-    public int getHp() { return 120; }
-    public int getAttack() { return 15; }
-}
--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/ClassiSpecifiche/ClasseGiocatore.java
+++
@@ -1,7 +0,0 @@
-package it.unicam.cs.mpgc.rpg125675.Modelli.ClassiSpecifiche;
-
-public abstract class ClasseGiocatore {
-
-    public int getHp() { return 120; }
-    public int getAttack() { return 15; }
-}
