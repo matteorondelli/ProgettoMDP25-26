4843545f0a6a15e5d0f388e724ef8cac42e304a7
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-05 16:40:44 +0200

    implementazione di un azione di cura

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Personaggio.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Personaggio.java
@@ -1,16 +1,19 @@
 package it.unicam.cs.mpgc.rpg125675.Modelli;
 
-import it.unicam.cs.mpgc.rpg125675.Modelli.ClassiSpecifiche.ClasseGiocatore;
+
 
 public class Personaggio extends Entità {
 
-    private int level;
-    private String nome;
+        public Personaggio(String nome, int puntiVita, int attacco) {
+            super(nome,puntiVita,attacco);
+        }
 
-    public Personaggio(String nome, ClasseGiocatore classeGiocatore) {
-        super(nome, classeGiocatore.getHp(), classeGiocatore.getAttack());
-        this.level = 1;
-    }
+        public void cura(){
+            this.setPuntiVita(Math.min(this.getPuntiVita() + 5, this.getPuntiVitaMassimi()));
+        }
+
+
+
 
 
 
