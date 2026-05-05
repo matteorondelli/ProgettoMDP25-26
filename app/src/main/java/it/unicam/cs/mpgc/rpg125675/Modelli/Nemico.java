671b2e0843aa090ffd90f45a93f7d97f0cf459db
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-05 16:40:59 +0200

    limatura dettagli

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Nemico.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Nemico.java
@@ -1,12 +1,8 @@
 package it.unicam.cs.mpgc.rpg125675.Modelli;
 
 public class Nemico extends Entità{
-    private String nome;
-    private int hp;
-    private int dmg;
 
-    public Nemico(String nome, int hp, int dmg) {
-        super(nome,hp,dmg);
-
+    public Nemico(String nome, int puntiVita, int attacco) {
+        super(nome,puntiVita,attacco);
     }
 }
