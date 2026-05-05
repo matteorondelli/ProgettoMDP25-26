59c6ac79336ea07dc409f1034c5ca79380e92ab7
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-05 16:41:40 +0200

    integrazione della classe GestioneCombattimento per la creazione di un primo prototipo funzionante

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Main.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Main.java
@@ -1,7 +1,21 @@
 package it.unicam.cs.mpgc.rpg125675;
 
+
+import it.unicam.cs.mpgc.rpg125675.Gestione.GestioneCombattimento;
+import it.unicam.cs.mpgc.rpg125675.Modelli.Nemico;
+import it.unicam.cs.mpgc.rpg125675.Modelli.Personaggio;
+
+import java.util.Scanner;
+
 public class Main {
-    static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        Personaggio giocatore = new Personaggio("Aldo", 100, 20);
+        Nemico nemico = new Nemico("Pdor", 100, 10);
+        Scanner scanner = new Scanner(System.in);
+
+        GestioneCombattimento gestioneCombattimento = new GestioneCombattimento(giocatore, nemico, scanner);
+        gestioneCombattimento.avvia();
     }
+
+
 }
