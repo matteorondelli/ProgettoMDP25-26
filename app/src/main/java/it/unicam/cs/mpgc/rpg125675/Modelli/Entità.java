ade26a607755cc6199dfd0027c379b9b9ccd38dc
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-05 16:39:17 +0200

    introduzione campo puntiVitaMassimi e migliorie ai vari getter e setter

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Entità.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Modelli/Entità.java
@@ -1,37 +1,50 @@
 package it.unicam.cs.mpgc.rpg125675.Modelli;
 
-
-
 public abstract class Entità {
 
     private String nome;
-    private int hp;
+    private int puntiVita;
+    private int puntiVitaMassimi;
     private int attacco;
 
-    public Entità(){}
 
-    public Entità(String nome, int hp, int attacco) {
+    public Entità(String nome, int puntiVita, int attacco) {
         this.nome = nome;
-        this.hp = hp;
+        this.puntiVita = puntiVita;
+        this.puntiVitaMassimi = puntiVita;
         this.attacco = attacco;
     }
 
-    public void attacca(Entità entità1, Entità entità2) {
-       entità2.setHp(entità2.getHp() - entità1.getAttacco());
+
+
+    public void attacca(Entità nemico){
+        nemico.setPuntiVita(Math.max(0, nemico.getPuntiVita() - this.getAttacco()));
     }
-    public boolean isAlive() {
-        return hp > 0;
+    public boolean vivo(){
+        return this.puntiVita > 0;
     }
-    public int getHp() {
-        return hp;
+
+
+
+    public int getPuntiVita() {
+        return puntiVita;
     }
-    public int setHp(int hp) {
-        this.hp = hp;
-        return hp;
+    public void setPuntiVita(int puntiVita) {
+        this.puntiVita = puntiVita;
+    }
+    public int getPuntiVitaMassimi() {
+        return puntiVitaMassimi;
     }
     public int getAttacco() {
         return attacco;
     }
+    public void setAttacco(int attacco) {
+        this.attacco = attacco;
+    }
     public String getNome() {
-        return nome; }
+        return nome;
+    }
+
 }
+
+
