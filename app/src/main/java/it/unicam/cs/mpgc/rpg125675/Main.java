dbe608ffde1acbb4773d49a96b025759b311c440
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-05-09 17:58:41 +0200

    aggiunta dell'uso della classe InterfacciaCombattimento

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/Main.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/Main.java
@@ -1,19 +1,25 @@
 package it.unicam.cs.mpgc.rpg125675;
 
 
-import it.unicam.cs.mpgc.rpg125675.Gestione.GestioneCombattimento;
-import it.unicam.cs.mpgc.rpg125675.Modelli.Nemico;
-import it.unicam.cs.mpgc.rpg125675.Modelli.Personaggio;
+import it.unicam.cs.mpgc.rpg125675.gestione.GestioneCombattimento;
+import it.unicam.cs.mpgc.rpg125675.interfaccia.InterfacciaCombattimento;
+import it.unicam.cs.mpgc.rpg125675.interfaccia.InterfacciaConsole;
+import it.unicam.cs.mpgc.rpg125675.modelli.Nemico;
+import it.unicam.cs.mpgc.rpg125675.modelli.Personaggio;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Personaggio giocatore = new Personaggio("Aldo", 100, 20);
         Nemico nemico = new Nemico("Pdor", 100, 10);
         Scanner scanner = new Scanner(System.in);
+        InterfacciaCombattimento interfaccia = new InterfacciaConsole(scanner);
 
-        GestioneCombattimento gestioneCombattimento = new GestioneCombattimento(giocatore, nemico, scanner);
+        GestioneCombattimento gestioneCombattimento = new GestioneCombattimento(giocatore, nemico, interfaccia);
+
+
         gestioneCombattimento.avvia();
     }
 
