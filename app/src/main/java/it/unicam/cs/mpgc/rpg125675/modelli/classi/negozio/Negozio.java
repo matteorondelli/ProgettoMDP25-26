dd306a215f3eaa9a51a11bddb647b0666165332b
Matteo Rondelli <matteo.rondelli@studenti.unicam.it>
2026-06-06 17:30:37 +0200

    aggiunta metodo acquista

--- app/src/main/java/it/unicam/cs/mpgc/rpg125675/modelli/classi/negozio/Negozio.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125675/modelli/classi/negozio/Negozio.java
@@ -1,8 +1,11 @@
 package it.unicam.cs.mpgc.rpg125675.modelli.classi.negozio;
 
+import it.unicam.cs.mpgc.rpg125675.modelli.classi.oggetti.Arma;
 import it.unicam.cs.mpgc.rpg125675.modelli.classi.oggetti.OggettoBase;
+import it.unicam.cs.mpgc.rpg125675.modelli.classi.personaggi.Giocatore;
 import it.unicam.cs.mpgc.rpg125675.modelli.classi.util.CaricaJson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Negozio {
@@ -13,7 +16,17 @@ public class Negozio {
         this.negozio = CaricaJson.caricaNegozio();
     }
 
+    public boolean acquista(Giocatore giocatore, OggettoBase oggetto) {
+        if (!negozio.contains(oggetto)) return false;
+        if (!giocatore.spendiOro(oggetto.getPrezzo())) return false;
+        giocatore.aggiungiOggetto(oggetto);
+        if (oggetto instanceof Arma arma) {
+            giocatore.equipaggiaArma(arma);
+        }
+        return true;
+    }
+
     public List<OggettoBase> getNegozio() {
-        return negozio;
+        return new ArrayList<>(negozio);
     }
 }
